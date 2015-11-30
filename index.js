e782d037607ea9dd0839733371f819c5cd7cae94
Enzo Matrix <alphanumeric@openmailbox.org>
2015-11-30 21:05:02 +0000

    Accessing Entries in an address book by Key value passed in a GET request

--- index.js
+++ index.js
@@ -1,65 +1,31 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+var entries = {
+  1: {
+    firstName: "John",
+    lastName: "Smith",
+    emails: [{
+      type: "home",
+      address: "john@smith.com"
+    }, {
+      type: "work",
+      address: "jsmith@megacorp.com"
+    }]
+  },
+  // ...
+};
 
-app.get('/hello', function(req, res) {
-  res.send('<html><head><title>Greetings!</title></head><body><h1>Hello World!</h1></body></html>');
-});
-
-app.get('/hello/:firstname', function(req, res) {
-  res.send('<h1>Hello ' + req.params.firstname + '!</h1>');
-  //console.log(req.params);
-});
-
-
-
-app.get('/op/:operation/:number1/:number2', function(req, res) {
-  var firstNum = parseFloat(req.params.number1);
-  var secNum = parseFloat(req.params.number2);
-
-  if (req.params.operation === 'add') {
-    var obj = {
-      "operator": 'add',
-      "firstOperand": firstNum,
-      "secondOperand": secNum,
-      "solution": (firstNum + secNum).toString()
-    }
-    res.send(obj);
-  }
-  else if (req.params.operation === 'subtract') {
-    obj = {
-      "operator": 'subtract',
-      "firstOperand": firstNum,
-      "secondOperand": secNum,
-      "solution": (firstNum - secNum).toString()
-    }
-    res.send(obj);
-  }
-  else if (req.params.operation === 'multiply') {
-    obj = {
-      "operator": 'multiply',
-      "firstOperand": firstNum,
-      "secondOperand": secNum,
-      "solution": (firstNum * secNum).toString()
-    }
-    res.send(obj);
-  }
-  else if (req.params.operation === 'divide') {
-    obj = {
-      "operator": 'divide',
-      "firstOperand": firstNum,
-      "secondOperand": secNum,
-      "solution": (firstNum / secNum).toString()
-    }
-    res.send(obj);
+app.get('/entries/:entryId', function(req, res) {
+  if (entries[Number(req.params.entryId)] === undefined) {
+    res.sendStatus(404);
   }
   else {
-    res.status(415).send('Unsupported Operand. Please use: "add, subtract, multiply or divide" only');
+    res.send(entries[Number(req.params.entryId)]);
   }
 });
+
+
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 

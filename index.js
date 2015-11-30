88b320511f9fe94b8c7d2f9f8f0b100843a5a547
Enzo Matrix <alphanumeric@openmailbox.org>
2015-11-30 23:27:42 +0000

    We can now POST data to our address book :)

--- index.js
+++ index.js
@@ -1,70 +1,102 @@
 var express = require('express');
 var app = express();
+var bpar = require('body-parser');
+app.use(bpar.json());
 
 var entries = {
-  1: {
-    firstName: "John",
-    lastName: "Smith",
-    emails: [{
-      type: "home",
-      address: "john@smith.com"
-    }, {
-      type: "work",
-      address: "jsmith@megacorp.com"
-    }]
-  },
-  2: {
-    firstName: "Cindy",
-    lastName: "Paramount",
-    emails: [{
-      type: "home",
-      address:  "cindy@home.com"
-    }, {
-      type: "work",
-      address: "misspara@minishop.com"
-    }]
-  },
-  3: {
-    firstName: "Daliah",
-    lastName: "Carmela",
-    emails: [{
-      type: "work",
-      address: "d.money@mammoth.com"
-    }, {
-      type: "other",
-      address: "basement@loveshack.com"
-    }]
-  },
+  
+  
+  // 1: {
+  //   firstName: "John",
+  //   lastName: "Smith",
+  //   emails: [{
+  //     type: "home",
+  //     address: "john@smith.com"
+  //   }, {
+  //     type: "work",
+  //     address: "jsmith@megacorp.com"
+  //   }]
+  // },
+  // 2: {
+  //   firstName: "Cindy",
+  //   lastName: "Paramount",
+  //   emails: [{
+  //     type: "home",
+  //     address: "cindy@home.com"
+  //   }, {
+  //     type: "work",
+  //     address: "misspara@minishop.com"
+  //   }]
+  // },
+  // 3: {
+  //   firstName: "Daliah",
+  //   lastName: "Carmela",
+  //   emails: [{
+  //     type: "work",
+  //     address: "d.money@mammoth.com"
+  //   }, {
+  //     type: "other",
+  //     address: "basement@loveshack.com"
+  //   }]
+  // },
   // ...
 };
 
-app.get('/entry/search', function(req, res) {
-  var matches = [];
-  console.log(req.query);
-  
-  for (var p in entries) {
-    if (
-      ((req.query.firstName !== undefined) && (entries[p].firstName.toLowerCase().indexOf((req.query.firstName).toLowerCase()) !== -1)) || 
-      ((req.query.lastName !== undefined) && (entries[p].lastName.toLowerCase().indexOf((req.query.lastName).toLowerCase()) !== -1))
-      ) {
-      matches.push(entries[p].firstName + ' ' + entries[p].lastName);
-    }
-    else if (req.query.emails) {
-      entries[p].emails.forEach(function(obj){
-        if (obj.address.toLowerCase().indexOf((req.query.emails).toLowerCase()) !== -1) {
-          matches.push(entries[p].firstName + ' ' + entries[p].lastName);
-        }
-      });
-    }
+// app.get('/entry/search', function(req, res) {
+//   var matches = [];
+//   console.log(req.query);
 
-  }
-  if (matches.length > 0){
-  res.send("The following entries contain a match: " + matches);
-  }
-  else {
-    res.send("Could not find any matches");
-  }
+//   for (var p in entries) {
+//     if (
+//       ((req.query.firstName !== undefined) && (entries[p].firstName.toLowerCase().indexOf((req.query.firstName).toLowerCase()) !== -1)) ||
+//       ((req.query.lastName !== undefined) && (entries[p].lastName.toLowerCase().indexOf((req.query.lastName).toLowerCase()) !== -1))
+//     ) {
+//       matches.push(entries[p].firstName + ' ' + entries[p].lastName);
+//     }
+//     else if (req.query.emails) {
+//       entries[p].emails.forEach(function(obj) {
+//         if (obj.address.toLowerCase().indexOf((req.query.emails).toLowerCase()) !== -1) {
+//           matches.push(entries[p].firstName + ' ' + entries[p].lastName);
+//         }
+//       });
+//     }
+
+//   }
+//   if (matches.length > 0) {
+//     res.send("The following entries contain a match: " + matches);
+//   }
+//   else {
+//     res.send("Could not find any matches");
+//   }
+// });
+
+
+
+//////////////////////////
+// BEGIN THEPOST SECTION//
+//////////////////////////
+
+app.post('/entry', function(req, res) {
+  console.log(req.body);
+  var bodyId = 1;
+  entries.id = bodyId;
+  entries.content = req.body;
+  res.send("You've created a new entry with ID: " + entries.id + ' ' + entries.content.firstName + ' ' + entries.content.lastName );
+  bodyId++;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
